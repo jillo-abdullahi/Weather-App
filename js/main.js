b0ad193931e70c0213fde01a3befc4d6628f25b8
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-07-22 23:06:31 +0300

    UI changes and added forecast weather

--- js/main.js
+++ js/main.js
@@ -1,24 +1,110 @@
 $(document).ready(function(){
 
-	var skycons = new Skycons({"color": "#000"});
-  // on Android, a nasty hack is needed: {"resizeClear": true}
+	
+	//Button on click to hide some stuff and show some other stuff :)
+	$("#getWeatherBtn").on("click", function(){
+		$("#weather-content").css("display","inline");
+		$(".landing-page").css("display","none");
 
-  // you can add a canvas by it's ID...
-  skycons.add("weather-icon", Skycons.PARTLY_CLOUDY_DAY);
+		getWeatherInfo();
+	});
 
-  // start animation!
-  skycons.play();
+	//Get current date and time from Moment:
+
+	$("#date").html( moment().format('MMMM Do YYYY, h:mm a') );
+	$("#day").html(moment().format('dddd') );
+
+
+
+	/*var skycons = new Skycons({"color": "#000"});
+  	skycons.add("weather-icon", Skycons.PARTLY_CLOUDY_DAY);
+  	skycons.play();*/
 
 
 	function getWeatherInfo(){
-		var uri = "https://api.darksky.net/forecast/e3d851489bc8e6c05e3debb0dcddeff5/37.8267,-122.4233";
-		$.get(uri, function(result){
 
-			console.log(result.summary);
-		});
+		//Get location
+		var lat;
+		var longi;
+
+		var options = {
+  			enableHighAccuracy: true,
+  			timeout: 5000,
+  			maximumAge: 0
+			};
+
+		function success(pos) {
+  			var crd = pos.coords;
+  			lat = crd.latitude;
+  			longi = crd.longitude;
+
+  			//Setting up some variables for use later
+			var key = "d46c15efe644412da4c135215172007";
+			var coords = lat+","+longi;
+			var weatheruri = "https://api.apixu.com/v1/current.json?key="+key+"&q="+coords;
+			var forecasturi = "https://api.apixu.com/v1/forecast.json?key="+key+"&q="+coords+"&days=5";
+
+			//Ajax call for current weather
+			$.getJSON(weatheruri, function(info){
+				console.log(info.current.temp_c);
+			});
+
+			//Ajax call for forecast conditions
+			$.getJSON(forecasturi, function(weather){
+
+			//Setting header variables for forecast days
+				var day1 = moment().add(1, 'd').format('dddd');
+				var day2 = moment().add(2, 'd').format('dddd');
+				var day3 = moment().add(3, 'd').format('dddd');
+				var day4 = moment().add(4, 'd').format('dddd');
+
+			//Grabbing items from json file
+				//icons
+				var icon1 = "https:"+weather.forecast.forecastday[1].day.condition.icon;
+				var icon2 = "https:"+weather.forecast.forecastday[2].day.condition.icon;			
+				var icon3 = "https:"+weather.forecast.forecastday[3].day.condition.icon;			
+				var icon4 = "https:"+weather.forecast.forecastday[4].day.condition.icon;
+				
+				//max temperature
+				var temp1 = weather.forecast.forecastday[1].day.maxtemp_c;
+				var temp2 = weather.forecast.forecastday[2].day.maxtemp_c;
+				var temp3 = weather.forecast.forecastday[3].day.maxtemp_c;
+				var temp4 = weather.forecast.forecastday[4].day.maxtemp_c;
+
+			//Displaying info
+				//Setting forecast days
+				$("#day1").text(day1);
+				$("#day2").text(day2);
+				$("#day3").text(day3);
+				$("#day4").text(day4);
+
+				//Setting icons
+				$("#icon1").attr('src',icon1);
+				$("#icon2").attr('src',icon2);
+				$("#icon3").attr('src',icon3);
+				$("#icon4").attr('src',icon4);
+
+				//maximum forecast temperatures
+				$("#temp1").html(temp1+"<span>&#8451;<span>");
+				$("#temp2").html(temp2+"<span>&#8451;<span>");
+				$("#temp3").html(temp3+"<span>&#8451;<span>");
+				$("#temp4").html(temp4+"<span>&#8451;<span>");
+
+			
+			});
+
+			};//End of success function
+
+			function error(err) {
+			  console.warn(`ERROR(${err.code}): ${err.message}`);
+			};
+
+			navigator.geolocation.getCurrentPosition(success, error, options);
+
+		
+
+		
 
 	}
-	$(".weather-btn").on("click",function(){
-		getWeatherInfo();
-	});
+	
 });
